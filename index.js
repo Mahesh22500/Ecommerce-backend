2473cd07c98e37b917492c22cd1b2e00707581fd
Mahesh22500 <maheshsingh22500@gmail.com>
2024-11-13 20:12:57 +0530

    mongo error

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const app = express();
 const main = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    // console.log("mongodb connected");
+    console.log("mongodb connected");
   } catch (err) {
-    // console.log(err);
+    console.log("connection err:",err);
   }
 };
 
